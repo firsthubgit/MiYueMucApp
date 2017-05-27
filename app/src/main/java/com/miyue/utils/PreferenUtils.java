012889ea8a2d1a80a788c342654d2a74f8aff10e
firsthubgit <1280209233@qq.com>
2017-05-27 10:57:06 +0800

    代码更新

---
+++ app/src/main/java/com/miyue/utils/PreferenUtils.java
@@ -0,0 +1,42 @@
+package com.miyue.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by zhangzhendong on 17/5/27.
+ */
+
+public class PreferenUtils {
+
+    public static final String BACKGROUND_PATH = "pic_background_path";
+
+    private static PreferenUtils sInstance;
+
+    private static SharedPreferences mPreferences;
+
+    public PreferenUtils(final Context context) {
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    public static final PreferenUtils getInstance(final Context context) {
+        if (sInstance == null) {
+            sInstance = new PreferenUtils(context.getApplicationContext());
+        }
+        return sInstance;
+    }
+
+    public void exit(){
+        final SharedPreferences.Editor editor = mPreferences.edit();
+        editor.putLong("exit_time", System.currentTimeMillis());
+        editor.commit();
+    }
+
+    public void putBgPath(String path){
+        final SharedPreferences.Editor editor = mPreferences.edit();
+        editor.putString(BACKGROUND_PATH, path);
+        editor.apply();
+    }
+
+}
