621886f04a6b00486449101581008f9fdac70bde
firsthubgit <nihaoweilai110@163.com>
2019-05-16 09:26:36 +0800

    更新项目

--- app/src/main/java/com/miyue/application/MiYueApp.java
+++ app/src/main/java/com/miyue/application/MiYueApp.java
@@ -1,10 +1,8 @@
 package com.miyue.application;
 
 import android.app.Application;
-import android.content.Intent;
 
 import com.greendao.DBHelper;
-import com.iflytek.cloud.SpeechUtility;
 
 /**
  * Created by zhangzhendong on 16/4/25.
@@ -23,8 +21,6 @@ public class MiYueApp extends Application {
         super.onCreate();
         instance = this;
         initData();
-        SpeechUtility.createUtility(MiYueApp.this, "appid=" + "5a2505ad");
-
     }
 
     private void initData(){
--- app/src/main/java/com/miyue/ui/fragment/main/SearchFragment.java
+++ app/src/main/java/com/miyue/ui/fragment/main/SearchFragment.java
@@ -28,22 +28,11 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.greendao.DBHelper;
 import com.greendao.SearchHis;
-import com.iflytek.cloud.ErrorCode;
-import com.iflytek.cloud.InitListener;
-import com.iflytek.cloud.RecognizerListener;
-import com.iflytek.cloud.RecognizerResult;
-import com.iflytek.cloud.SpeechConstant;
-import com.iflytek.cloud.SpeechError;
-import com.iflytek.cloud.SpeechRecognizer;
-import com.iflytek.cloud.ui.RecognizerDialog;
-import com.iflytek.cloud.ui.RecognizerDialogListener;
 import com.miyue.R;
 import com.miyue.application.MiYueConstans;
 import com.miyue.bean.QQSong;
 import com.miyue.bean.SongsInfo;
-import com.miyue.common.base.BaseActivity;
 import com.miyue.common.base.BaseMediaFragment;
 import com.miyue.http.Downkey;
 import com.miyue.http.HttpApi;
@@ -82,7 +71,7 @@ public class SearchFragment extends BaseMediaFragment implements OnLineMusicAdap
     private ImageView iv_recognize_sound;
 
     // 语音听写对象
-    private SpeechRecognizer mIat;
+//    private SpeechRecognizer mIat;
 
     /**历史记录实体类*/
     private List<SearchHis> mSeaList;
@@ -107,7 +96,7 @@ public class SearchFragment extends BaseMediaFragment implements OnLineMusicAdap
 
     private String mEngineType;
     // 语音听写UI
-    private RecognizerDialog mIatDialog;
+//    private RecognizerDialog mIatDialog;
 
     // 用HashMap存储听写结果
     private HashMap<String, String> mIatResults = new LinkedHashMap<String, String>();
@@ -118,12 +107,12 @@ public class SearchFragment extends BaseMediaFragment implements OnLineMusicAdap
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        // 引擎类型
-        mEngineType = SpeechConstant.TYPE_CLOUD;
-        // 初始化听写Dialog，如果只使用有UI听写功能，无需创建SpeechRecognizer
-        // 使用UI听写功能，请根据sdk文件目录下的notice.txt,放置布局文件和图片资源
-        mIatDialog = new RecognizerDialog(mActivity, mInitListener);
-        mIat = SpeechRecognizer.createRecognizer(mActivity, mInitListener);
+//        // 引擎类型
+//        mEngineType = SpeechConstant.TYPE_CLOUD;
+//        // 初始化听写Dialog，如果只使用有UI听写功能，无需创建SpeechRecognizer
+//        // 使用UI听写功能，请根据sdk文件目录下的notice.txt,放置布局文件和图片资源
+//        mIatDialog = new RecognizerDialog(mActivity, mInitListener);
+//        mIat = SpeechRecognizer.createRecognizer(mActivity, mInitListener);
         getHistroy();
     }
 
@@ -134,12 +123,12 @@ public class SearchFragment extends BaseMediaFragment implements OnLineMusicAdap
         super.onAttach(activity);
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
             // 引擎类型
-            mEngineType = SpeechConstant.TYPE_CLOUD;
+//            mEngineType = SpeechConstant.TYPE_CLOUD;
 
             // 初始化听写Dialog，如果只使用有UI听写功能，无需创建SpeechRecognizer
             // 使用UI听写功能，请根据sdk文件目录下的notice.txt,放置布局文件和图片资源
-            mIatDialog = new RecognizerDialog(mActivity, mInitListener);
-            mIat = SpeechRecognizer.createRecognizer(mActivity, mInitListener);
+//            mIatDialog = new RecognizerDialog(mActivity, mInitListener);
+//            mIat = SpeechRecognizer.createRecognizer(mActivity, mInitListener);
 
             getHistroy();
         }
@@ -186,21 +175,21 @@ public class SearchFragment extends BaseMediaFragment implements OnLineMusicAdap
                 mActivity.showText("请开始说话111...");
                 // 设置参数
                 boolean isShowDialog = true;
-                setParam();
-                if (isShowDialog) {
-                    // 显示听写对话框
-                    mIatDialog.setListener(mRecognizerDialogListener);
-                    mIatDialog.show();
-                    mActivity.showText("请开始说话...");
-                } else {
-                    // 不显示听写对话框
-                    ret = mIat.startListening(mRecognizerListener);
-                    if (ret != ErrorCode.SUCCESS) {
-                        mActivity.showText("听写失败,错误码：" + ret);
-                    } else {
-                        mActivity.showText("请开始说话…");
-                    }
-                }
+//                setParam();
+//                if (isShowDialog) {
+//                    // 显示听写对话框
+//                    mIatDialog.setListener(mRecognizerDialogListener);
+//                    mIatDialog.show();
+//                    mActivity.showText("请开始说话...");
+//                } else {
+//                    // 不显示听写对话框
+//                    ret = mIat.startListening(mRecognizerListener);
+//                    if (ret != ErrorCode.SUCCESS) {
+//                        mActivity.showText("听写失败,错误码：" + ret);
+//                    } else {
+//                        mActivity.showText("请开始说话…");
+//                    }
+//                }
             }
         });
 
@@ -476,160 +465,160 @@ public class SearchFragment extends BaseMediaFragment implements OnLineMusicAdap
     }
 
 
-    /**
-     * 初始化监听器。
-     */
-    private InitListener mInitListener = new InitListener() {
-
-        @Override
-        public void onInit(int code) {
-            Log.d(TAG, "SpeechRecognizer init() code = " + code);
-            if (code != ErrorCode.SUCCESS) {
-                mActivity.showText("初始化失败，错误码：" + code);
-            }
-        }
-    };
-
-
-    /**
-     * 参数设置
-     *
-     * @return
-     */
-    public void setParam() {
-        // 清空参数
-        mIat.setParameter(SpeechConstant.PARAMS, null);
-
-        // 设置听写引擎
-        mIat.setParameter(SpeechConstant.ENGINE_TYPE, mEngineType);
-        // 设置返回结果格式
-        mIat.setParameter(SpeechConstant.RESULT_TYPE, "json");
-
-        // 设置语言
-        mIat.setParameter(SpeechConstant.LANGUAGE, "zh_cn");
-        // 设置语言区域
-        mIat.setParameter(SpeechConstant.ACCENT, "mandarin");
+//    /**
+//     * 初始化监听器。
+//     */
+//    private InitListener mInitListener = new InitListener() {
+//
+//        @Override
+//        public void onInit(int code) {
+//            Log.d(TAG, "SpeechRecognizer init() code = " + code);
+//            if (code != ErrorCode.SUCCESS) {
+//                mActivity.showText("初始化失败，错误码：" + code);
+//            }
+//        }
+//    };
 
 
-        // 设置语音前端点:静音超时时间，即用户多长时间不说话则当做超时处理
-        mIat.setParameter(SpeechConstant.VAD_BOS, "4000");
+//    /**
+//     * 参数设置
+//     *
+//     * @return
+//     */
+//    public void setParam() {
+//        // 清空参数
+//        mIat.setParameter(SpeechConstant.PARAMS, null);
+//
+//        // 设置听写引擎
+//        mIat.setParameter(SpeechConstant.ENGINE_TYPE, mEngineType);
+//        // 设置返回结果格式
+//        mIat.setParameter(SpeechConstant.RESULT_TYPE, "json");
+//
+//        // 设置语言
+//        mIat.setParameter(SpeechConstant.LANGUAGE, "zh_cn");
+//        // 设置语言区域
+//        mIat.setParameter(SpeechConstant.ACCENT, "mandarin");
+//
+//
+//        // 设置语音前端点:静音超时时间，即用户多长时间不说话则当做超时处理
+//        mIat.setParameter(SpeechConstant.VAD_BOS, "4000");
+//
+//        // 设置语音后端点:后端点静音检测时间，即用户停止说话多长时间内即认为不再输入， 自动停止录音
+//        mIat.setParameter(SpeechConstant.VAD_EOS, "1000");
+//
+//        // 设置标点符号,设置为"0"返回结果无标点,设置为"1"返回结果有标点
+//        mIat.setParameter(SpeechConstant.ASR_PTT, "1");
+//
+//        // 设置音频保存路径，保存音频格式支持pcm、wav，设置路径为sd卡请注意WRITE_EXTERNAL_STORAGE权限
+//        // 注：AUDIO_FORMAT参数语记需要更新版本才能生效
+//        mIat.setParameter(SpeechConstant.AUDIO_FORMAT,"wav");
+//        mIat.setParameter(SpeechConstant.ASR_AUDIO_PATH, Environment.getExternalStorageDirectory()+"/msc/iat.wav");
+//    }
 
-        // 设置语音后端点:后端点静音检测时间，即用户停止说话多长时间内即认为不再输入， 自动停止录音
-        mIat.setParameter(SpeechConstant.VAD_EOS, "1000");
-
-        // 设置标点符号,设置为"0"返回结果无标点,设置为"1"返回结果有标点
-        mIat.setParameter(SpeechConstant.ASR_PTT, "1");
-
-        // 设置音频保存路径，保存音频格式支持pcm、wav，设置路径为sd卡请注意WRITE_EXTERNAL_STORAGE权限
-        // 注：AUDIO_FORMAT参数语记需要更新版本才能生效
-        mIat.setParameter(SpeechConstant.AUDIO_FORMAT,"wav");
-        mIat.setParameter(SpeechConstant.ASR_AUDIO_PATH, Environment.getExternalStorageDirectory()+"/msc/iat.wav");
-    }
-
-    /**
-     * 听写UI监听器
-     */
-    private RecognizerDialogListener mRecognizerDialogListener = new RecognizerDialogListener() {
-        public void onResult(RecognizerResult results, boolean isLast) {
-            printResult(results);
-        }
-
-        /**
-         * 识别回调错误.
-         */
-        public void onError(SpeechError error) {
-            mActivity.showText(error.getPlainDescription(true));
-        }
-
-    };
-
-    private void printResult(RecognizerResult results) {
-        String text = JsonParser.parseIatResult(results.getResultString());
-
-        String sn = null;
-        // 读取json结果中的sn字段
-        try {
-            JSONObject resultJson = new JSONObject(results.getResultString());
-            sn = resultJson.optString("sn");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        //存起来
-        mIatResults.put(sn, text);
-
-        StringBuffer resultBuffer = new StringBuffer();
-        for (String key : mIatResults.keySet()) {
-            resultBuffer.append(mIatResults.get(key));
-        }
-        if(!StringUtils.isNullOrEmpty(resultBuffer.toString())){
-            et_find_music.setText(resultBuffer.toString());
-            et_find_music.setSelection(et_find_music.length());
-
-            iv_recognize_sound.setVisibility(View.GONE);
-            iv_clear_edit.setVisibility(View.VISIBLE);
-        }
-    }
-
-    /**
-     * 听写监听器。
-     */
-    private RecognizerListener mRecognizerListener = new RecognizerListener() {
-
-        @Override
-        public void onBeginOfSpeech() {
-            // 此回调表示：sdk内部录音机已经准备好了，用户可以开始语音输入
-            mActivity.showText("开始说话");
-        }
-
-        @Override
-        public void onError(SpeechError error) {
-            // Tips：
-            // 错误码：10118(您没有说话)，可能是录音机权限被禁，需要提示用户打开应用的录音权限。
-            // 如果使用本地功能（语记）需要提示用户开启语记的录音权限。
-            mActivity.showText(error.getPlainDescription(true));
-
-        }
-
-        @Override
-        public void onEndOfSpeech() {
-            // 此回调表示：检测到了语音的尾端点，已经进入识别过程，不再接受语音输入
-            mActivity.showText("结束说话");
-        }
-
-        @Override
-        public void onResult(RecognizerResult results, boolean isLast) {
-            Log.d(TAG, results.getResultString());
-            printResult(results);
-            if (isLast) {
-                // TODO 最后的结果
-            }
-        }
-
-        @Override
-        public void onVolumeChanged(int volume, byte[] data) {
-            mActivity.showText("当前正在说话，音量大小：" + volume);
-            Log.d(TAG, "返回音频数据："+data.length);
-        }
-
-        @Override
-        public void onEvent(int eventType, int arg1, int arg2, Bundle obj) {
-            // 以下代码用于获取与云端的会话id，当业务出错时将会话id提供给技术支持人员，可用于查询会话日志，定位出错原因
-            // 若使用本地能力，会话id为null
-            //	if (SpeechEvent.EVENT_SESSION_ID == eventType) {
-            //		String sid = obj.getString(SpeechEvent.KEY_EVENT_SESSION_ID);
-            //		Log.d(TAG, "session id =" + sid);
-            //	}
-        }
-    };
+//    /**
+//     * 听写UI监听器
+//     */
+//    private RecognizerDialogListener mRecognizerDialogListener = new RecognizerDialogListener() {
+//        public void onResult(RecognizerResult results, boolean isLast) {
+//            printResult(results);
+//        }
+//
+//        /**
+//         * 识别回调错误.
+//         */
+//        public void onError(SpeechError error) {
+//            mActivity.showText(error.getPlainDescription(true));
+//        }
+//
+//    };
+//
+//    private void printResult(RecognizerResult results) {
+//        String text = JsonParser.parseIatResult(results.getResultString());
+//
+//        String sn = null;
+//        // 读取json结果中的sn字段
+//        try {
+//            JSONObject resultJson = new JSONObject(results.getResultString());
+//            sn = resultJson.optString("sn");
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//        //存起来
+//        mIatResults.put(sn, text);
+//
+//        StringBuffer resultBuffer = new StringBuffer();
+//        for (String key : mIatResults.keySet()) {
+//            resultBuffer.append(mIatResults.get(key));
+//        }
+//        if(!StringUtils.isNullOrEmpty(resultBuffer.toString())){
+//            et_find_music.setText(resultBuffer.toString());
+//            et_find_music.setSelection(et_find_music.length());
+//
+//            iv_recognize_sound.setVisibility(View.GONE);
+//            iv_clear_edit.setVisibility(View.VISIBLE);
+//        }
+//    }
+//
+//    /**
+//     * 听写监听器。
+//     */
+//    private RecognizerListener mRecognizerListener = new RecognizerListener() {
+//
+//        @Override
+//        public void onBeginOfSpeech() {
+//            // 此回调表示：sdk内部录音机已经准备好了，用户可以开始语音输入
+//            mActivity.showText("开始说话");
+//        }
+//
+//        @Override
+//        public void onError(SpeechError error) {
+//            // Tips：
+//            // 错误码：10118(您没有说话)，可能是录音机权限被禁，需要提示用户打开应用的录音权限。
+//            // 如果使用本地功能（语记）需要提示用户开启语记的录音权限。
+//            mActivity.showText(error.getPlainDescription(true));
+//
+//        }
+//
+//        @Override
+//        public void onEndOfSpeech() {
+//            // 此回调表示：检测到了语音的尾端点，已经进入识别过程，不再接受语音输入
+//            mActivity.showText("结束说话");
+//        }
+//
+//        @Override
+//        public void onResult(RecognizerResult results, boolean isLast) {
+//            Log.d(TAG, results.getResultString());
+//            printResult(results);
+//            if (isLast) {
+//                // TODO 最后的结果
+//            }
+//        }
+//
+//        @Override
+//        public void onVolumeChanged(int volume, byte[] data) {
+//            mActivity.showText("当前正在说话，音量大小：" + volume);
+//            Log.d(TAG, "返回音频数据："+data.length);
+//        }
+//
+//        @Override
+//        public void onEvent(int eventType, int arg1, int arg2, Bundle obj) {
+//            // 以下代码用于获取与云端的会话id，当业务出错时将会话id提供给技术支持人员，可用于查询会话日志，定位出错原因
+//            // 若使用本地能力，会话id为null
+//            //	if (SpeechEvent.EVENT_SESSION_ID == eventType) {
+//            //		String sid = obj.getString(SpeechEvent.KEY_EVENT_SESSION_ID);
+//            //		Log.d(TAG, "session id =" + sid);
+//            //	}
+//        }
+//    };
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if( null != mIat ){
+//        if( null != mIat ){
             // 退出时释放连接
-            mIat.cancel();
-            mIat.destroy();
-        }
+//            mIat.cancel();
+//            mIat.destroy();
+//        }
     }
 
 
